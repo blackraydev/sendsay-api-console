8f945c2f4625289fb1d42e899882a295bdff83d9
Ramil <black2001ray@mail.ru>
2021-10-27 14:41:21 +0300

    Configured paths for pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
@@ -10,15 +10,19 @@ import ConsolePage from './pages/ConsolePage';
 const { store, persistor } = createStore();
 
 const App = () => {
+  useEffect(() => {
+    console.log(process.env.PUBLIC_URL);
+  }, []);
+
   return (
     <Router>
       <Provider store={store}>
         <PersistGate loading={null} persistor={persistor}>
           <Switch>
-            <Route path={`${process.env.PUBLIC_URL}/console`}>
+            <Route path={process.env.PUBLIC_URL + '/console'}>
               <ConsolePage />
             </Route>
-            <Route path={`${process.env.PUBLIC_URL}/`}>
+            <Route path={process.env.PUBLIC_URL + '/'}>
               <LoginPage />
             </Route>
           </Switch>
