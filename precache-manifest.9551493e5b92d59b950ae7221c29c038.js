5478608b252348a1c30609c41ab9e607fe5387ea
Ramil <black2001ray@mail.ru>
2021-10-27 09:05:11 +0300

    Updates

---
+++ precache-manifest.9551493e5b92d59b950ae7221c29c038.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc4e95480a1de90ab50645fd6acec770",
+    "url": "/sendsay-api-console/index.html"
+  },
+  {
+    "revision": "be4e86e88e29b09e1347",
+    "url": "/sendsay-api-console/static/css/main.d29117dd.chunk.css"
+  },
+  {
+    "revision": "1099e4339ef2a6c9fc38",
+    "url": "/sendsay-api-console/static/js/2.44a5e476.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/sendsay-api-console/static/js/2.44a5e476.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be4e86e88e29b09e1347",
+    "url": "/sendsay-api-console/static/js/main.57db0293.chunk.js"
+  },
+  {
+    "revision": "d64c2831dabeba405b8f",
+    "url": "/sendsay-api-console/static/js/runtime-main.161718cb.js"
+  }
+]);
