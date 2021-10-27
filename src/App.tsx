78112975e51eda9adf3f1a5895c039b43987a4cf
Ramil <black2001ray@mail.ru>
2021-10-27 14:48:19 +0300

    Configured paths for pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { HashRouter as Router, Switch, Route } from 'react-router-dom';
@@ -10,10 +10,6 @@ import ConsolePage from './pages/ConsolePage';
 const { store, persistor } = createStore();
 
 const App = () => {
-  useEffect(() => {
-    console.log(process.env.PUBLIC_URL);
-  }, []);
-
   return (
     <Router>
       <Provider store={store}>
